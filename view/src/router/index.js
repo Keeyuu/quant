18b97bb7083334c1b1b01726bf889a80cd32f540
keeYuc <52500690@qq.com>
2021-12-05 01:16:11 +0800

    yes

--- view/src/router/index.js
+++ view/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Line from '../views/Line'
-
 Vue.use(VueRouter)
 
 const routes = [
--- view/src/views/Line.vue
+++ view/src/views/Line.vue
@@ -1,6 +1,23 @@
 <template>
+
 <div
     class="k-line-chart-container">
+    <div id=choice>
+      <input type="text" v-model="code" placeholder="edit me">
+      <input type="text" v-model="level" placeholder="edit me">
+      <button
+        v-on:click="select_code()">
+      select
+      </button>
+      <button
+        v-for="{ key, text } in shapes"
+        :key="key"
+        v-on:click="setShapeType(key)"
+      >
+        {{ text }}
+      </button>
+      <button v-on:click="removeAllShape()">清除</button>
+    </div>
       <div id="draw-shape-k-line"
       class="k-line-chart"/>
 </div>
@@ -8,17 +25,26 @@
 
 <script>
 import { init } from 'klinecharts'
+import axios from 'axios'
 const data = require('./demo.json')
-
 const L1_COLOR = '#2196F3'
 const L2_COLOR = '#FF9600'
 
 export default {
   data () {
     return {
+      code: data.code,
+      level: 'day',
+      shapes: [
+        { key: 'priceLine', text: '价格线' },
+        { key: 'priceChannelLine', text: '价格通道线' },
+        { key: 'parallelStraightLine', text: '平行直线' },
+        { key: 'fibonacciLine', text: '斐波那契回调' }
+      ]
     }
   },
   mounted () {
+    console.log()
     this.kLineChart = init('draw-shape-k-line')
     this.kLineChart.applyNewData(data.souce_data)
     this.kLineChart.createTechnicalIndicator('VOL')
@@ -35,6 +61,16 @@ export default {
 
       // draw L2 line
       this.drawLine(l1Arr, L2_COLOR)
+    },
+    select_code () {
+      console.log(this.code)
+      axios.get('http://localhost:8000/result?code=' + this.code + '&level=' + this.level)
+    },
+    setShapeType: function (shapeName) {
+      this.kLineChart.createShape(shapeName)
+    },
+    removeAllShape () {
+      this.kLineChart.removeShape()
     },
     drawLine (arr, color) {
       arr.forEach(l => {
